570573fbd6ced232935ee5e52f90fda8d2c7ff2a
Nicolas Bukovits <nicobuk@gmail.com>
2021-11-13 21:38:18 -0300

    primera parte de descriptores finalizada

--- parser.py
+++ parser.py
@@ -1,5 +1,6 @@
 from ply import lex
 from ply import yacc
+import sys
 
 # -----------------------------------------------------------------------------
 # calc.py
@@ -8,21 +9,16 @@ from ply import yacc
 # -----------------------------------------------------------------------------
 
 tokens = (
-    'NAME','NUMBER',
-    'PLUS','MINUS','TIMES','DIVIDE','EQUALS',
-    'LPAREN','RPAREN',
+    'LCORCHETE','RCORCHETE',
+    'DESCRIPTOR','DESCRIPTORVALOR'
     )
 
 # Tokens
 
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_TIMES   = r'\*'
-t_DIVIDE  = r'/'
-t_EQUALS  = r'='
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
-t_NAME    = r'[a-zA-Z_][a-zA-Z0-9_]*'
+t_LCORCHETE    = r'\['
+t_RCORCHETE    = r'\]'
+t_DESCRIPTOR    = r'[a-zA-Z0-9\s]+'
+t_DESCRIPTORVALOR    = r'\"[a-zA-Z0-9\s]+\"'
 
 def t_NUMBER(t):
     r'\d+'
@@ -30,7 +26,7 @@ def t_NUMBER(t):
     return t
 
 # Ignored characters
-t_ignore = " \t"
+t_ignore = ' \t'
 
 def t_newline(t):
     r'\n+'
@@ -42,39 +38,44 @@ def t_error(t):
 
 # Build the lexer
 import ply.lex as lex
-lex.lex()
+lexer = lex.lex()
 
 # Precedence rules for the arithmetic operators
-precedence = (
-    ('left','PLUS','MINUS'),
-    ('left','TIMES','DIVIDE'),
-    ('right','UMINUS'),
-    )
+#precedence = (
+#    ('left','PLUS','MINUS'),
+#    ('left','TIMES','DIVIDE'),
+#    ('right','UMINUS'),
+#    )
 
 # dictionary of names (for storing variables)
-names = { }
+#names = { }
 
-def p_statement_assign(p):
-    'statement : NAME EQUALS expression'
-    names[p[1]] = p[3]
+def p_linea_descriptor(p):
+    '''descriptor_evento : LCORCHETE DESCRIPTOR DESCRIPTORVALOR RCORCHETE descriptor_evento 
+                         | empty'''
+    #names[p[1]] = p[3]
 
-def p_statement_expr(p):
-    'statement : expression'
-    print(p[1])
+def p_empty(p):
+    'empty :'
+    pass
+#def p_statement_expr(p):
+#    'statement : expression'
+#    print(p[1])
 
-def p_expression_binop(p):
-    '''expression : expression PLUS expression
-                  | expression MINUS expression
-                  | expression TIMES expression
-                  | expression DIVIDE expression'''
-    if p[2] == '+'  : p[0] = p[1] + p[3]
-    elif p[2] == '-': p[0] = p[1] - p[3]
-    elif p[2] == '*': p[0] = p[1] * p[3]
-    elif p[2] == '/': p[0] = p[1] / p[3]
+#def p_expression_binop(p):
+#    '''expression : expression PLUS expression
+#                  | expression MINUS expression
+#                  | expression TIMES expression
+#                  | expression DIVIDE expression'''
+#    if p[2] == '+'  : p[0] = p[1] + p[3]
+#    elif p[2] == '-': p[0] = p[1] - p[3]
+#    elif p[2] == '*': p[0] = p[1] * p[3]
+#    elif p[2] == '/': p[0] = p[1] / p[3]
 
-def p_expression_uminus(p):
-    'expression : MINUS expression %prec UMINUS'
+''' def p_expression_uminus(p):
+   'expression : MINUS expression %prec UMINUS'
     p[0] = -p[2]
+
 
 def p_expression_group(p):
     'expression : LPAREN expression RPAREN'
@@ -92,15 +93,28 @@ def p_expression_name(p):
         print(f"Undefined name {p[1]!r}")
         p[0] = 0
 
+'''
 def p_error(p):
     print(f"Syntax error at {p.value!r}")
 
 import ply.yacc as yacc
 yacc.yacc()
 
-while True:
-    try:
-        s = input('calc > ')
-    except EOFError:
-        break
-    yacc.parse(s)
+
+#lex.runmain()
+
+data = sys.stdin.read()
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+'''while True:
+    tok = lexer.token()
+    if not tok:
+        break      # No more input
+    print(tok)
+'''
+yacc.parse(data)
+
+    
