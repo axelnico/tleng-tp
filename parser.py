be0693c27f9acd7b83fe653145e02b3f8cdd4c6d
Nicolas Bukovits <nicobuk@gmail.com>
2021-11-17 19:19:12 -0300

    avances

--- parser.py
+++ parser.py
@@ -10,14 +10,13 @@ import sys
 
 tokens = (
     'DESCRIPTOR', 'LLLAVE', 'RLLAVE', 'LPAREN', 'RPAREN','RES','NUMBER','MOVIMIENTO',
-    'SIMBOLO', 'TEXTO', 'CONTINUAJUGADA'
+    'SIMBOLO', 'TEXTO', 'CONTINUAJUGADA', 
     )
 
 # Tokens
 
 t_DESCRIPTOR    = r'\[[^\[\]]+ \"[^\[\]]+\"\]'
 t_RES                = r'(1-0|0-1|1\/2-1\/2)'
-t_MOVIMIENTO        = r'((P|N|B|R|Q|K)?[a-h]?[1-8]?x?[a-h][1-8])|O-O-O|O-O'
 t_SIMBOLO           = r'\+|\+\+|\!|\?'
 
 comentarios_abiertos  = 0
@@ -26,6 +25,11 @@ comentarios_abiertos  = 0
 states = (
     ('insideComment','exclusive'),
 )
+
+def t_insideComment_INITIAL_MOVIMIENTO(t):
+    r'((P|N|B|R|Q|K)?[a-h]?[1-8]?x?[a-h][1-8])|O-O-O|O-O'
+    return t
+
 
 def t_insideComment_INITIAL_LLLAVE(t):
     r'{'
@@ -73,13 +77,13 @@ def t_NUMBER(t):
 
 # Ignored characters
 t_ignore = ' \t'
-t_insideComment_ignore = ''
+t_insideComment_ignore = ' '
 
 def t_insideComment_TEXTO(t):
-    r'[^\{\}\(\)]+'
+    r'[^\{\}\(\)\s]+'
     return t
 
-def t_newline(t):
+def t_insideComment_INITIAL_newline(t):
     r'\n+'
     t.lexer.lineno += t.value.count("\n")
 
@@ -115,7 +119,7 @@ def p_inicio_pgn(p):
 def p_partida(p):
     '''partida : DESCRIPTOR descriptor NUMBER desc_jugada jugada resultado partida
                | empty'''
-    if len(p) == 3:
+    if len(p) > 2:
         if p[3] != p[4] and p[4] is not None:
             raise Exception("Continuacion de jugada incorrecta")
     if len(p) > 2 and  p[3] != 1:
@@ -124,16 +128,15 @@ def p_partida(p):
 def p_linea_descriptor(p):
     '''descriptor : DESCRIPTOR descriptor
                   | empty'''
-    #names[p[1]] = p[3]
 
 def p_jugada(p):
     '''jugada : NUMBER desc_jugada jugada
               | empty'''
     p[0] = p[1]
-    if len(p) == 3:
+    if len(p) == 4:
         if p[1] != p[2] and p[2] is not None:
             raise Exception("Continuacion de jugada incorrecta")
-    if len(p) == 3 and not p[3] is None:
+    if len(p) == 4 and p[3] is not None:
         if p[1] != p[3] - 1:
             raise Exception("Los numeros de jugada no son secuenciales")
 
@@ -144,7 +147,6 @@ def p_desc_jugada(p):
     #p[2] = p[0] + 1
     #if(p[2] != p[0] + 1):
     #    raise Exception("Los numeros de jugada no son secuenciales")
-    #names[p[1]] = p[3]
 
 def p_comentario_con_siguiente_jugada(p):
     '''comentario_siguiente_jugada : LLLAVE contenido RLLAVE CONTINUAJUGADA
@@ -167,9 +169,14 @@ def p_comentario(p):
                   | empty'''
 
 def p_contenido(p):
-    '''contenido : contenido TEXTO contenido 
+    '''contenido : contenido texto contenido 
                  | comentario
                  | empty'''
+
+def p_texto(p):
+    '''texto : TEXTO texto
+             | MOVIMIENTO texto
+             | empty'''
     
 def p_resultado(p):
     '''resultado : RES'''
@@ -177,42 +184,7 @@ def p_resultado(p):
 def p_empty(p):
     'empty :'
     pass
-#def p_statement_expr(p):
-#    'statement : expression'
-#    print(p[1])
 
-#def p_expression_binop(p):
-#    '''expression : expression PLUS expression
-#                  | expression MINUS expression
-#                  | expression TIMES expression
-#                  | expression DIVIDE expression'''
-#    if p[2] == '+'  : p[0] = p[1] + p[3]
-#    elif p[2] == '-': p[0] = p[1] - p[3]
-#    elif p[2] == '*': p[0] = p[1] * p[3]
-#    elif p[2] == '/': p[0] = p[1] / p[3]
-
-''' def p_expression_uminus(p):
-   'expression : MINUS expression %prec UMINUS'
-    p[0] = -p[2]
-
-
-def p_expression_group(p):
-    'expression : LPAREN expression RPAREN'
-    p[0] = p[2]
-
-def p_expression_number(p):
-    'expression : NUMBER'
-    p[0] = p[1]
-
-def p_expression_name(p):
-    'expression : NAME'
-    try:
-        p[0] = names[p[1]]
-    except LookupError:
-        print(f"Undefined name {p[1]!r}")
-        p[0] = 0
-
-'''
 def p_error(p):
     if p is None:
         print("Parsing error")
